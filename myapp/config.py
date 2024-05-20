d8464bce8c379d305b165eb63e1482c7d3d78661
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2024-05-20 20:11:56 +0200

    Fixed expanduser lol

--- myapp/config.py
+++ myapp/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     def __init__(self):
-        self.config = json.load(open(os.expanduser("~/.config/remarkacord/config.json")))
+        self.config = json.load(open(os.path.expanduser("~/.config/remarkacord/config.json")))
 
     def get(self, key):
         return self.config.get(key)
